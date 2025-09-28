bc2b993e6012eeaab907de36ecc599453e947b2a
Stepan <korablikiuu@gmail.com>
2025-09-28 23:16:19 +0300

    fixed error, also finished formed components

--- ecommerce-admin/app/layout.tsx
+++ ecommerce-admin/app/layout.tsx
@@ -1,26 +1,28 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
-import { ClerkProvider } from '@clerk/nextjs'
+import {Inter } from 'next/font/google';
+import "./globals.css";
+import { ClerkProvider } from '@clerk/nextjs';
+import { ModalProvider } from '@/providers/modal-provider';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: 'Admin Dashboard',
-  description: 'Admin Dashboard',
-}
+  title: "Admin Dashboard",
+  description: "Admin Dashboard",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
+    <ClerkProvider>
     <html lang="en">
       <body className={inter.className}>
-        <ClerkProvider>
-          {children}
-        </ClerkProvider>
-      </body>
+        <ModalProvider />
+        {children} 
+        </body>
     </html>
-  )
+    </ClerkProvider>
+  );
 }
